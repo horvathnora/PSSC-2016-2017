f2c4e1d3e707f277a8a50aa40df0d852e48b444f
horvathnora <nora_horvath@yahoo.com>
2016-10-21 17:22:31 +0300

    Tema 1

--- PSSC/Models/Class1.cs
+++ PSSC/Models/Class1.cs
@@ -6,8 +6,71 @@ using System.Threading.Tasks;
 
 namespace Models
 {
-    public class Class1
+    enum tip { examen, distribuita };
+
+    public class Profesor
     {
-        //remove or alter this class, then implement your models.  
+        List<Disciplina> listaDiscipline = new List<Disciplina>();
+        List<Student> listaStudenti = new List<Student>();
+
+        public string numeProfesor { get; set; }
+
+        public Profesor(string numeProfesor)
+        {
+            this.numeProfesor = numeProfesor;
+
+        }
+
+    }
+    public class Disciplina:Profesor
+    {
+
+        tip tipDisciplina;
+        public string numeDisciplina { get; set; }
+        public int crediteDisciplina { get; set; }
+
+        public Disciplina(string numeDisciplina,int crediteDisciplina,string numeProfesor):base(numeProfesor)
+        {
+            this.numeDisciplina = numeDisciplina;
+            this.crediteDisciplina = crediteDisciplina;
+            this.numeProfesor = numeProfesor;
+        }
+
+        
+    }
+
+    public class Student:Situatie
+    {
+        List<Disciplina> listaDiscipline = new List<Disciplina>();
+
+        public string nume { get; set; }
+        public int an { get; set; }
+        public int nrMatricol { get; set; }
+       
+
+        public Student(string nume,int an,int nrMatricol)
+        {
+            this.nume = nume;
+            this.an = an;
+            this.nrMatricol = nrMatricol;
+            
+        }
+    }
+
+  
+    public class Decanat
+    {
+        List<Disciplina> listaDiscipline=new List<Disciplina>();
+        List<Student> listaStudenti = new List<Student>();
+
+    }
+
+    public class Situatie
+    {
+        public int credite { get; set; }
+        public float[] nota_examen { get; set; }
+        public int prezenta { get; set; }
+        public float[] note_activitate { get; set; }
+
     }
 }
